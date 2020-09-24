d068d533d12009cfd670077b18a1f85ef429aef2
Marek Kaliszczyk <marek.kaliszczyk@gmail.com>
2020-09-25 01:42:19 +0200

    fixed ssl scans

--- do_ssl_scans.sh
+++ do_ssl_scans.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-help() { 
-	echo -e "Usage:\n ./do_ssl_scans.sh MODE (--all, --sslscan, --testssl)\n\nneeds gnmap output in pwd to run\n\noutput in sslscan/ and testssl/ dirs" 
-} 
+help() {
+    echo -e "Usage:\n ./do_ssl_scans.sh MODE (--all, --sslscan, --testssl)\n\nneeds gnmap output in pwd to run\n\noutput in sslscan/ and testssl/ dirs"
+}
 
-if [ $# -ne 1 ]; then 
-	help
-	exit 1
-fi 
-
-MODE=$1
 #proper path for tools here
 TESTSSL=testssl
-SSLSCAN=sslscan
+SSLSCAN=/home/marek/tools/sslscan/sslscan
 
 GNMAP=$(find . -maxdepth 1 -iname "*.gnmap")
 if [[ -z "$GNMAP" ]]; then
- echo "[-] No gnmap files, exiting"
- exit 2
+    echo "[-] No gnmap files, exiting"
+    exit 1
 fi
-
 
 run_sslscan() {
     if [ ! -d $(pwd)/sslscan ]; then
@@ -28,11 +21,16 @@ run_sslscan() {
     for ssl in $(pwd)/*.ssl; do
         cat $ssl | while read line || [[ -n $line ]];
         do
-                $SSLSCAN --no-colour $line > sslscan/$line.sslcan
-                cat sslscan/$line.sslcan | sed -n '/Supported Server Cipher(s):$/,/Server Key Exchange Group(s):$/p' | sed -e '1d' -e '$d' | head -n -1 | awk '{print $2, $5}' > sslscan/$line.report.sslscan.out
-                echo "[+] Done sslscan for $line"
+            $SSLSCAN --no-colour $line > sslscan/$line.sslcan
+            cat sslscan/$line.sslcan | sed -n '/Supported Server Cipher(s):$/,/Server Key Exchange Group(s):$/p' | sed -e '1d' -e '$d' | head -n -1 | awk '{print $2, $5}' > sslscan/$line.sslscan.report.out
+            echo "[+] Done sslscan for $line"
         done
-    done 
+    done
+    for file in sslscan/*; do
+        if [[ ! -s $file ]]; then
+        rm $file
+        fi
+    done    
 }
 
 run_testssl() {
@@ -42,36 +40,38 @@ run_testssl() {
     for ssl in $(pwd)/*.ssl; do
         cat $ssl | while read line || [[ -n $line ]];
         do
-                $TESTSSL --full --logfile testssl/$line.testssl $line 
-                echo "[+] Done testssl.sh for $line"
+            $TESTSSL --full --logfile testssl/$line.testssl $line
+            echo "[+] Done testssl.sh for $line"
         done
-    done 
+    done
 }
-
-while getopts ":h" option; do
-   case $option in
-      h) 
-         help
-         exit;;  
-   esac
-done
 
 for gnmap in $(pwd)/*.gnmap; do
     cat $gnmap | awk '{for(i=1;i<=NF;i++){if ($i ~ /ssl/){print $2":"$i}}}' | awk -F "/" '{print $1}' > $(basename $gnmap).ssl
 done
 
-case "$MODE" in
---all)
-	run_sslscan
-    run_testssl
-	break;;
---sslscan)
-	run_sslscan
-	break;;
---testssl)
-	run_testssl
-	break;;
-*) 
-    echo "Error: Invalid option"
-    exit;;    		
-esac
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+    -h | --help )
+        help
+        exit
+    ;;
+    -a | --all )
+        run_sslscan
+        run_testssl
+        exit
+    ;;
+    --sslscan )
+        run_sslscan
+        exit
+    ;;
+    --testssl)
+        run_testssl
+        exit
+    ;;
+    *)
+        echo "Error. Invalid option"
+        help
+        exit 1
+    ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
