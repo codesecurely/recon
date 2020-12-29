df8da53220daf413a3d60d29f2f92df81c724675
codesecurely <marek.kaliszczyk@gmail.com>
2020-12-29 05:45:40 +0100

    fixed output file naming and added PlantUML source

--- recon.sh
+++ recon.sh
@@ -41,47 +41,47 @@ run_large_amass() {
 
 run_amass() {
 	echo "AMASS"
-	if [ -s "$DOMAIN/hosts-amass-$DOMAIN.txt" ]; then
+	if [ -s "$DOMAIN/hosts-amass.txt" ]; then
 		return
 	fi
 	echo "[STARTING AMASS]"
-	amass enum -active -brute -src -o $DOMAIN/out-amass-$DOMAIN.txt -aw $WORDLIST -bl $BLACKLIST -d $DOMAIN -config config.ini
+	amass enum -active -brute -src -o $DOMAIN/out-amass.txt -aw $WORDLIST -bl $BLACKLIST -d $DOMAIN -config config.ini
 	echo "[AMASS DONE]"
-	cat $DOMAIN/out-amass-$DOMAIN.txt | cut -d']' -f 2 | awk '{print $1}' | sort -u > $DOMAIN/hosts-amass-$DOMAIN.txt
-	sed $SED $WORDLIST > $DOMAIN/hosts-wordlist-$DOMAIN.txt
-	cat $DOMAIN/hosts-amass-$DOMAIN.txt $DOMAIN/hosts-wordlist-$DOMAIN.txt > $DOMAIN/hosts-all-$DOMAIN.txt
+	cat $DOMAIN/out-amass.txt | cut -d']' -f 2 | awk '{print $1}' | sort -u > $DOMAIN/hosts-amass.txt
+	sed $SED $WORDLIST > $DOMAIN/hosts-wordlist.txt
+	cat $DOMAIN/hosts-amass.txt $DOMAIN/hosts-wordlist.txt > $DOMAIN/hosts-all.txt
 	if [ -n "$EXISTING_LIST" ]; then
-		cat $EXISTING_LIST >> $DOMAIN/hosts-all-$DOMAIN.txt
-		cat $DOMAIN/hosts-all-$DOMAIN.txt | sort -u > $DOMAIN/hosts-all-$DOMAIN.txt
+		cat $EXISTING_LIST >> $DOMAIN/hosts-all.txt
+		cat $DOMAIN/hosts-all.txt | sort -u > $DOMAIN/hosts-all.txt
 	fi	
 
 }
 
 run_ldns_walk() {
-	if [ -s "$DOMAIN/hosts-ldnswalk-$DOMAIN.txt" ]; then
+	if [ -s "$DOMAIN/hosts-ldnswalk.txt" ]; then
 		return
 	fi
 	
 	for i in $(echo $DOMAIN | tr "," "\n")
 	do
-		ldns-walk $i | awk '{print $1}' >> $DOMAIN/hosts-ldnswalk-$DOMAIN.txt
+		ldns-walk $i | awk '{print $1}' >> $DOMAIN/hosts-ldnswalk.txt
 	done
 	echo "[APPENDING WORDLIST TO DOMAIN FOR BRUTE FORCE]"
 	for i in $(echo $DOMAIN | tr "," "\n")
 	do
-	sed s/$/.$i/ $WORDLIST >> $DOMAIN/hosts-wordlist-$DOMAIN.txt	
+	sed s/$/.$i/ $WORDLIST >> $DOMAIN/hosts-wordlist.txt	
 	done
 	echo "[LDNS-WALK DONE]"
-	cat $DOMAIN/hosts-ldnswalk-$DOMAIN.txt >> $DOMAIN/hosts-all-$DOMAIN.txt
+	cat $DOMAIN/hosts-ldnswalk.txt >> $DOMAIN/hosts-all.txt
 
 }
 
 run_massdns() {
-	if [ -s "$DOMAIN/ips-online-$DOMAIN.txt" ]; then
+	if [ -s "$DOMAIN/ips-online.txt" ]; then
 		return
 	fi
-	massdns --root -r resolvers.txt -t A -o S -w $DOMAIN/massdns-$DOMAIN.out $DOMAIN/hosts-all-$DOMAIN.txt
-	cat $DOMAIN/massdns-$DOMAIN.out | awk '{print $3}' | sort -u | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > $DOMAIN/ips-online-$DOMAIN.txt
+	massdns --root -r resolvers.txt -t A -o S -w $DOMAIN/massdns.txt $DOMAIN/hosts-all.txt
+	cat $DOMAIN/massdns.txt | awk '{print $3}' | sort -u | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > $DOMAIN/ips-online.txt
 }
 
 run_masscan() {
@@ -89,48 +89,48 @@ run_masscan() {
 		echo "[!] This script must be launched as root."
 		exit 1
 	fi
-	if [ -s "$DOMAIN/nmap_targets-$DOMAIN.tmp" ]; then
+	if [ -s "$DOMAIN/nmap_targets.tmp" ]; then
 		return
 	fi
-	masscan -iL $DOMAIN/ips-online-$DOMAIN.txt --rate 1000000 -p1-65535 --open -oX $DOMAIN/masscan-$DOMAIN.xml
-	open_ports=$(cat $DOMAIN/masscan-$DOMAIN.xml | grep portid | cut -d "\"" -f 10 | sort -n | uniq | paste -sd,)
-	echo $open_ports > $DOMAIN/open-ports-$DOMAIN.txt
-	cat $DOMAIN/masscan-$DOMAIN.xml | grep portid | cut -d "\"" -f 4 | sort -V | uniq > $DOMAIN/nmap_targets-$DOMAIN.tmp
+	masscan -iL $DOMAIN/ips-online.txt --rate 1000000 -p1-65535 --open -oX $DOMAIN/masscan.xml
+	open_ports=$(cat $DOMAIN/masscan.xml | grep portid | cut -d "\"" -f 10 | sort -n | uniq | paste -sd,)
+	echo $open_ports > $DOMAIN/open-ports.txt
+	cat $DOMAIN/masscan.xml | grep portid | cut -d "\"" -f 4 | sort -V | uniq > $DOMAIN/nmap_targets.tmp
 }
 
 run_httpx() {
-	if [ ! -s "$DOMAIN/massdns-$DOMAIN.out" ]; then
-		echo "no file $DOMAIN/massdns-$DOMAIN.out, exiting"
+	if [ ! -s "$DOMAIN/massdns.txt" ]; then
+		echo "no file $DOMAIN/massdns.txt, exiting"
 		return
 	fi
 
-	if [ -s "$DOMAIN/webservers-live-domains-$DOMAIN.txt" ]; then
+	if [ -s "$DOMAIN/webservers-live.txt" ]; then
 		echo "httpx already done, skipping"
 		return
 	fi
 
 	#sort by third unique column (IP), then print first column (domain)
-	cat $DOMAIN/massdns-$DOMAIN.out | sort -u -t ' ' -k 3,3 | awk '{print $1}' | sed -r 's/\.$//' | httpx -ports 80,81,443,591,2082,2087,2095,2096,3000,8000,8001,8008,8080,8083,8443,8834,8888  > $DOMAIN/webservers-live-domains-$DOMAIN.txt
+	cat $DOMAIN/massdns.txt | sort -u -t ' ' -k 3,3 | awk '{print $1}' | sed -r 's/\.$//' | httpx -ports 80,81,443,591,2082,2087,2095,2096,3000,8000,8001,8008,8080,8083,8443,8834,8888  > $DOMAIN/webservers-live.txt
 }
 
 run_gau() {
 	for i in $(echo $DOMAIN | tr "," "\n")
 	do
-	gau -providers wayback,otx,commoncrawl $i | grep -v -E "(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|svg)" >> $DOMAIN/spidered-content-$DOMAIN.tmp 
+	gau -providers wayback,otx,commoncrawl $i | grep -v -E "(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|svg)" >> $DOMAIN/spidered-content.tmp 
 	done 
 }
 
 run_gospider() {
-	if [ ! -s "$DOMAIN/webservers-live-domains-$DOMAIN.txt" ]; then
+	if [ ! -s "$DOMAIN/webservers-live.txt" ]; then
 		return
 	fi
 	#quiet, threads=2, concurrent=5, depth=3	
-	gospider -S $DOMAIN/webservers-live-domains-$DOMAIN.txt -q -t 2 -c 5 -d 3 --blacklist jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico,svg > $DOMAIN/spidered-content-$DOMAIN.tmp
-	if [ -s "$DOMAIN/gobuster-endpoints-$DOMAIN.txt" ]; then
-		gospider -S $DOMAIN/gobuster-endpoints-$DOMAIN.txt -q -t 2 -c 5 -d 3 --blacklist jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico,svg >> $DOMAIN/spidered-content-$DOMAIN.tmp
+	gospider -S $DOMAIN/webservers-live.txt -q -t 2 -c 5 -d 3 --blacklist jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico,svg > $DOMAIN/spidered-content.tmp
+	if [ -s "$DOMAIN/gobuster-endpoints.txt" ]; then
+		gospider -S $DOMAIN/gobuster-endpoints.txt -q -t 2 -c 5 -d 3 --blacklist jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico,svg >> $DOMAIN/spidered-content.tmp
 	fi
 	run_gau
-	cat $DOMAIN/spidered-content-$DOMAIN.tmp | grep -o -E "(([a-zA-Z][a-zA-Z0-9+-.]*\:\/\/)|mailto|data\:)([a-zA-Z0-9\.\&\/\?\:@\+-\_=#%;,])*" | sort -u | qsreplace -a > $DOMAIN/spidered-content-$DOMAIN.txt
+	cat $DOMAIN/spidered-content.tmp | grep -o -E "(([a-zA-Z][a-zA-Z0-9+-.]*\:\/\/)|mailto|data\:)([a-zA-Z0-9\.\&\/\?\:@\+-\_=#%;,])*" | sort -u | qsreplace -a > $DOMAIN/spidered-content.txt
 }
 
 run_nmap() {
@@ -138,15 +138,15 @@ run_nmap() {
 		echo "[!] This script must be launched as root."
 		exit 1
 	fi
-	if [ -s "$DOMAIN/nmap_results-$DOMAIN.xml" ]; then
+	if [ -s "$DOMAIN/nmap_results.xml" ]; then
 		return
 	fi
-	open_ports=$(cat $DOMAIN/masscan-$DOMAIN.xml | grep portid | cut -d "\"" -f 10 | sort -n | uniq | wc -l)
+	open_ports=$(cat $DOMAIN/masscan.xml | grep portid | cut -d "\"" -f 10 | sort -n | uniq | wc -l)
 	
 	if [ $open_ports -le 100 ]; then
-		nmap -Pn -sC -sV -p $(cat $DOMAIN/open-ports-$DOMAIN.txt) -iL $DOMAIN/nmap_targets-$DOMAIN.tmp -oA $DOMAIN/nmap_results-$DOMAIN
+		nmap -Pn -sC -sV -p $(cat $DOMAIN/open-ports.txt) -iL $DOMAIN/nmap_targets.tmp -oA $DOMAIN/nmap_results
 	else
-		nmap -Pn -sC -sV -p- -v -iL $DOMAIN/nmap_targets-$DOMAIN.tmp -oA $DOMAIN/nmap_results-$DOMAIN
+		nmap -Pn -sC -sV -p- -v -iL $DOMAIN/nmap_targets.tmp -oA $DOMAIN/nmap_results
 	fi
 }
 
@@ -154,26 +154,26 @@ run_aquatone() {
 	if [ -d "$DOMAIN/aquatone" ]; then
 		return
 	fi
-	cat $DOMAIN/nmap_results-$DOMAIN.xml | aquatone -scan-timeout 900 -nmap -out $DOMAIN/aquatone/	
+	cat $DOMAIN/nmap_results.xml | aquatone -scan-timeout 900 -nmap -out $DOMAIN/aquatone/	
 }
 
 run_gobuster_recurse() {
 	LEVEL=$1
-	if [ ! -e "$DOMAIN/webservers-live-domains-$DOMAIN.txt" ] || [ -s "$DOMAIN/gobuster-endpoints-$DOMAIN.txt" ]; then
+	if [ ! -e "$DOMAIN/webservers-live.txt" ] || [ -s "$DOMAIN/gobuster-endpoints.txt" ]; then
 		return
 	fi
 	#[[ -n $line ]] so it doesn't skip last line if there's no empty newline
-	cat $DOMAIN/webservers-live-domains-$DOMAIN.txt | while read line || [[ -n $line ]];
+	cat $DOMAIN/webservers-live.txt | while read line || [[ -n $line ]];
 	do
-		$(pwd)/gobuster_recurse.sh $line "$WORDLIST_DIR/common.txt" $LEVEL $DOMAIN/gobuster-endpoints-$DOMAIN.txt
+		$(pwd)/gobuster_recurse.sh $line "$WORDLIST_DIR/common.txt" $LEVEL $DOMAIN/gobuster-endpoints.txt
 	done 
 }
 
 run_gobuster_vhosts() {
-	if [ ! -e "$DOMAIN/webservers-live-domains-$DOMAIN.txt" ] || [ -s "$DOMAIN/gobuster-vhosts-all.txt" ]; then
+	if [ ! -e "$DOMAIN/webservers-live.txt" ] || [ -s "$DOMAIN/gobuster-vhosts-all.txt" ]; then
 		return
 	fi
-	cat $DOMAIN/webservers-live-domains-$DOMAIN.txt | while read line || [[ -n $line ]];
+	cat $DOMAIN/webservers-live.txt | while read line || [[ -n $line ]];
 	do
 		gobuster vhost -u $line -w "$WORDLIST_DIR/common-vhosts.txt" -o $DOMAIN/gobuster-vhosts.tmp
 		cat $DOMAIN/gobuster-vhosts.tmp >> $DOMAIN/gobuster-vhosts-all.txt		
@@ -182,19 +182,19 @@ run_gobuster_vhosts() {
 }
 
 run_gobuster_files() {
-	cat $DOMAIN/webservers-live-domains-$DOMAIN.txt | while read line || [[ -n $line ]];
+	cat $DOMAIN/webservers-live.txt | while read line || [[ -n $line ]];
 	do
 		gobuster dir -s 200,204 -u $line -w "$WORDLIST_DIR/raft-large-files.txt" -t 10 -o $DOMAIN/gobuster-files.tmp
-		cat $DOMAIN/gobuster-files.tmp >> $DOMAIN/gobuster-files-$DOMAIN.txt
+		cat $DOMAIN/gobuster-files.tmp >> $DOMAIN/gobuster-files.txt
 	done
-	if [ -e "$DOMAIN/gobuster-endpoints-$DOMAIN.txt" ]; then
-		cat $DOMAIN/gobuster-endpoints-$DOMAIN.txt | sort -u | while read line || [[ -n $line ]];
+	if [ -e "$DOMAIN/gobuster-endpoints.txt" ]; then
+		cat $DOMAIN/gobuster-endpoints.txt | sort -u | while read line || [[ -n $line ]];
 		do
 			gobuster dir -u $line -w "$WORDLIST_DIR/raft-large-files.txt" -t 10 -o $DOMAIN/gobuster-files.tmp
-			cat $DOMAIN/gobuster-files.tmp >> $DOMAIN/gobuster-files-$DOMAIN.txt 
+			cat $DOMAIN/gobuster-files.tmp >> $DOMAIN/gobuster-files.txt 
 		done
 	fi
-	cat $DOMAIN/gobuster-files-$DOMAIN.txt | sort -u > $DOMAIN/gobuster-files-$DOMAIN.txt
+	cat $DOMAIN/gobuster-files.txt | sort -u > $DOMAIN/gobuster-files.txt
 	rm $DOMAIN/gobuster-files.tmp
 }
 
